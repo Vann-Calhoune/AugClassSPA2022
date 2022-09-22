88ae564f2bc82aaf8ffe66f6d01e166ee30fa0cf
Vann Calhoune <vanncalhoune@Vanns-Air.lan>
2022-09-21 21:19:54 -0500

    AUG22SPA

--- index.js
+++ index.js
@@ -13,13 +13,18 @@ function render(state = store.Home) {
       ${Footer()}
     `;
 
+  afterRender(state);
+
   router.updatePageLinks();
 }
 
+function afterRender(state) {
+  document.querySelector(".fa-bars").addEventListener("click", () => {
+    document.querySelector("nav > ul").classList.toggle("hidden--mobile");
+  });
+}
+
 // add menu toggle to bars icon in nav bar
-// document.querySelector(".fa-bars").addEventListener("click", () => {
-//   document.querySelector("nav > ul").classList.toggle("hidden--mobile");
-// });
 
 router
   .on({
